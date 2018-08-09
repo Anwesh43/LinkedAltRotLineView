7b25003ae317c4093adc9cedc958329d092584d4
Anwesh43 <anweshthecool0@gmail.com>
2018-08-10 01:01:28 +0530

    adding State class

--- altrotlineview/src/main/java/com/anwesh/uiprojects/altrotlineview/AltRotLineView.kt
+++ altrotlineview/src/main/java/com/anwesh/uiprojects/altrotlineview/AltRotLineView.kt
@@ -23,9 +23,29 @@ class AltRotLineView(ctx : Context) : View(ctx) {
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += 0.01f * this.dir
+            if (Math.abs(this.scale - this.prevScale) > 1) {
+                this.scale = this.prevScale + this.dir
+                this.dir = 0f
+                this.prevScale = this.scale
+                cb(this.scale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (this.dir == 0f) {
+                this.dir = 1 - 2 * this.prevScale
+                cb()
+            }
+        }
     }
 }
