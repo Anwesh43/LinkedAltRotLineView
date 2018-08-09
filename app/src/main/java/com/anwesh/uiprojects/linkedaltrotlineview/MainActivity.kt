c8744051d622837660304bc078e8bb98766ae243
Anwesh43 <anweshthecool0@gmail.com>
2018-08-10 01:43:46 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedaltrotlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedaltrotlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedaltrotlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.altrotlineview.AltRotLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AltRotLineView.create(this)
     }
 }
