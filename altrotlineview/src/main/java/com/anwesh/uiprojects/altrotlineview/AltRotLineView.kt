020c17ac7c4b8241f3138a97ea4f70b4bf6b615e
Anwesh43 <anweshthecool0@gmail.com>
2018-08-10 01:29:18 +0530

    adding LinkedARL class

--- altrotlineview/src/main/java/com/anwesh/uiprojects/altrotlineview/AltRotLineView.kt
+++ altrotlineview/src/main/java/com/anwesh/uiprojects/altrotlineview/AltRotLineView.kt
@@ -137,4 +137,30 @@ class AltRotLineView(ctx : Context) : View(ctx) {
             next?.draw(canvas, paint)
         }
     }
+
+    data class LinkedARL(var i : Int) {
+
+        private var root : ARLNode = ARLNode(0)
+
+        private var curr : ARLNode = root
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
